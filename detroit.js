8b27eab6a2317a57092a366a2957a30240f768d5
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-06 10:12:15 -0500

    ok i dont need these files

--- detroit.js
+++ detroit.js
@@ -1,1 +1,12 @@
-alert(stats)
+if(!localStorage.getItem("detroit_stats")) {
+    localStorage.setItem("detroit_stats", {});
+}
+
+var stats = JSON.parse(localStorage.getItem("detroit_stats"));
+
+if(stats.detroit != 1) {
+    stats = {
+        detroit: 1,
+        inventory: []
+    }
+}
--- settings.js
+++
@@ -1,2 +0,0 @@
-// You can edit this file to make the detroit settings cooler, or make the game easier to play
-// https://vscode.dev
