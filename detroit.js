ae675523e8838c719f58710af7a471318e723770
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-06 10:56:42 -0500

    ok what did i do......

--- detroit.js
+++ detroit.js
@@ -1,21 +1,83 @@
 if(!localStorage.getItem("detroit_stats")) {
-    localStorage.setItem("detroit_stats", {});
+    localStorage.setItem("detroit_stats", "detroit:10;inv:item1,item2;name:PlayerName;iq:0;att:0;def:0;h:100;m:0");
 }
 
-var stats = JSON.parse(localStorage.getItem("detroit_stats"));
+var objectStats = {};
+var statString = localStorage.getItem("detroit_stats");
+statString.split(";").forEach(stat => {
+objectStats[stat.split(":")[0]] = stat.split(":")[1]
+})
 
-if(stats.detroit != 10) {
-    stats = {
-        detroit: 10,
-        inventory: [],
-        player: {
-            name: "Player"+Math.random().toString().substring(2,6)
-        },
-        iq: 0,
-        attack: 0,
-        defense: 0,
-        health: 100
+var gameDiv = document.getElementById("game");
+var statsDiv = document.getElementById("stats");
+updateStats();
+function updateStats() {
+    statsDiv.innerHTML = `
+<p>Debug: ${objectStats.detroit}</p>
+<p>Money: ${objectStats.m}</p>
+<p>Inventory: ${objectStats.inv}</p>
+<p>Player Name: ${objectStats.name}</p>
+<p>IQ: ${objectStats.iq}</p>
+<p>Attack: ${objectStats.att}</p>
+<p>Defense: ${objectStats.def}</p>
+<p>Health: ${objectStats.h}</p>`;
+c = `detroit:${objectStats.detroit};inv:${objectStats.inv};name:${objectStats.name};iq:${objectStats.iq};att:${objectStats.att};def:${objectStats.def};h:${objectStats.h};m:${objectStats.m}`
+localStorage.setItem("detroit_stats", c)
+}
+function createActionButton(action, callback) {
+    a = document.createElement("button")
+    a.appendChild(document.createTextNode(action))
+    a.id = "action:"+action
+    document.body.appendChild(a)
+    callback(a);
+    return a;
+}
+
+createActionButton("Debug: Raise IQ", (button) => {
+    button.addEventListener("click", (event) => {
+        objectStats.iq ++;
+        updateStats();
+    })
+})
+
+function openShop() {
+    if(document.getElementById("shop")) {
+        document.getElementById("shop").remove();
+        return;
+    }
+    gameDiv.innerHTML += `<div id="shop"><h1>Shop</h1></div>`
+    addShopItem({name:"Cool Test Item",cost:50})
+}
+
+function addShopItem(item={name:"My Item", cost:5}, callback=(button)=>{}) {
+    b = document.createElement('button')
+    b.appendChild(document.createTextNode(item.name+": D$"+item.cost))
+    document.getElementById("shop").appendChild(b)
+    b.onclick = function (event) {
+        buyItemFromShop(item);
+    }
+    callback(b);
+    return b;
+}
+
+function buyItemFromShop(item) {
+    if(objectStats.m >= item.cost) {
+        alert("bought")
+        objectStats.m = objectStats.m - item.cost;
+        objectStats.inv += ","+item.name
+        updateStats();
     }
 }
 
-localStorage.setItem("detroit_stats", JSON.parse(stats));
+createActionButton("Debug: Toggle Shop", (button) => {
+    button.addEventListener("click", (event) => {
+        openShop();
+    })
+})
+
+createActionButton("Debug: Raise money", (button) => {
+    button.onclick = (event) => {
+        objectStats.m = 15000;
+        updateStats();
+    }
+})
