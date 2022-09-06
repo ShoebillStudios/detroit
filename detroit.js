ab74b0c6d0ec84f03ca3d64d7efd180d040866cd
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-06 10:16:46 -0500

    stats

--- detroit.js
+++ detroit.js
@@ -7,6 +7,15 @@ var stats = JSON.parse(localStorage.getItem("detroit_stats"));
 if(stats.detroit != 1) {
     stats = {
         detroit: 1,
-        inventory: []
+        inventory: [],
+        player: {
+            name: "Player"+Math.random().toString().substring(2,6)
+        },
+        iq: 0,
+        attack: 0,
+        defense: 0,
+        health: 100
     }
 }
+
+alert(stats)
