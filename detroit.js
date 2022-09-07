a7e4cfb687512addb636919e4772304a36912217
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-07 11:48:30 -0500

    i know there's capitals in ohio code but IT DOESNT MATTER IT WORKS OKAY

--- detroit.js
+++ detroit.js
@@ -1,9 +1,10 @@
 var version = "1.0";
+var build = "-dev";
 if(!localStorage.getItem("detroit_stats")) {
     prompts = ['Welcome to Detroit. What will we call you?', 'This is the beginning of your adventure in Detroit. What shall we call you?', 'Welcome to Detroit. What is your name?']
     newname = prompt(prompts[Math.floor(Math.random() * prompts.length)])
     alert("Welcome to Detroit, "+newname+".");
-    localStorage.setItem("detroit_stats", "detroit:"+version+";inv:House Key;name:"+newname+";iq:0;att:0;def:0;h:100;m:0;hwid:"+Math.random().toString().substring(2,5)+";loc:Your Home (Detroit);job:None");
+    localStorage.setItem("detroit_stats", "detroit:"+version+";inv:House Key;name:"+newname+";iq:0;att:0;def:0;h:100;m:20;hwid:"+Math.random().toString().substring(2,5)+";loc:Your Home (Detroit);job:None;at:Detroit");
 }
 
 var chances = {
@@ -15,7 +16,6 @@ var statString = localStorage.getItem("detroit_stats");
 statString.split(";").forEach(stat => {
 objectStats[stat.split(":")[0]] = stat.split(":")[1]
 })
-
 if(objectStats.detroit != version) {
     alert("Your save is from an older version. Updating your save")
 }
@@ -27,13 +27,12 @@ function updateStats() {
     statsDiv.innerHTML = `
 <p>Job: ${objectStats.job}</p>
 <p>Money: ${objectStats.m}</p>
-<p>Inventory: ${objectStats.inv}</p>
 <p>Location: ${objectStats.loc}</p>
 <p>IQ: ${objectStats.iq}</p>
 <p>Attack: ${objectStats.att}</p>
 <p>Defense: ${objectStats.def}</p>
 <p>Health: ${objectStats.h}</p>`;
-c = `detroit:${objectStats.detroit};inv:${objectStats.inv};name:${objectStats.name};iq:${objectStats.iq};att:${objectStats.att};def:${objectStats.def};h:${objectStats.h};m:${objectStats.m};hwid:${objectStats.hwid};loc:${objectStats.loc};job:${objectStats.job}`
+c = `detroit:${objectStats.detroit};inv:${objectStats.inv};name:${objectStats.name};iq:${objectStats.iq};att:${objectStats.att};def:${objectStats.def};h:${objectStats.h};m:${objectStats.m};hwid:${objectStats.hwid};loc:${objectStats.loc};job:${objectStats.job};at:${objectStats.at}`
 localStorage.setItem("detroit_stats", c)
 }
 function createActionButton(action, callback) {
@@ -53,26 +52,31 @@ function openShop() {
         document.getElementById("shop").remove();
         objectStats.loc = "Your Home (Detroit)"
         updateStats();
+        if(objectStats.at == "Ohio") {
+            ExitOhioShop();
+        }
         return;
     }
-    if(!objectStats.loc.includes("Detroit")) {
-        newLocation(objectStats.loc);
-    }
     gameDiv.innerHTML += `<div id="shop"><h1>Shop</h1></div>`
+    if(objectStats.at == "Ohio") {
+        OhioShop();
+        return;
+    }
     addShopItem({name:"Ticket to Ohio",cost:50,limit:1}, (event) => {
         alert("You're going to Ohio.")
         objectStats.loc = "Ohio";
+        objectStats.at = "Ohio";
         updateStats();
         weGoinToOhio();
     })
-    addShopItem({name:"Job at FloorStore",cost:1,limit:1}, (event) => {
+    addShopItem({name:"Job at FloorStore",cost:10,limit:1}, (event) => {
         alert("Congratulations, you got the job.")
         objectStats.job = "FloorStore";
         updateStats();
     })
     addShopItem({name:"Balazation 5",cost:500,limit:2}, (event) => {
         alert("YOOO YOU GOT THE BALASTATION 5...")
-        createActionButton("Play your Balazation5", (button) => {
+        createActionButton("Play your Balazation5 "+Math.random().toString().substring(2,3), (button) => {
             button.onclick = function (event) {
                 alert("You've won the DutyCal Tournament!!!")
                 objectStats.m = +objectStats.m + 5000;
@@ -122,6 +126,8 @@ function buyItemFromShop(item, callback) {
     }
 }
 
+// Actions at the bottom of the screen
+
 createActionButton("Go to Shop", (button) => {
     button.addEventListener("click", (event) => {
         objectStats.loc = "Shop (Detroit)"
@@ -129,7 +135,61 @@ createActionButton("Go to Shop", (button) => {
         updateStats();
     })
 })
-
+createActionButton("View Inventory", (button) => {
+    button.onclick = (ev) => {
+        ivl = [];
+    objectStats.inv.split(',').forEach(iv => {
+        ivl.push(iv);
+    })
+    alert("Your Inventory\n\n"+ivl.join("\n"))
+    }
+})
+objectStats.inv.split(",").forEach(invItem => {
+    if(invItem == "Ticket to Ohio") {
+        if(objectStats.at == "Detroit") {
+        createActionButton("Go to Ohio", (bev) => {
+            bev.onclick = (ev) => {
+                alert("You're going to Ohio.")
+                objectStats.loc = "Ohio";
+                objectStats.at = "Ohio";
+                updateStats();
+                window.location.reload();
+            }
+        })
+    }
+    }
+    if(invItem == "Ticket back to Detroit") {
+        if(objectStats.at == "Ohio") {
+            createActionButton("Go to Detroit", (bev) => {
+                bev.onclick = (ev) => {
+                    alert("You're going to Detroit.")
+                    objectStats.loc = "Detroit";
+                    objectStats.at = "Detroit";
+                    updateStats();
+                    window.location.reload();
+                }
+            })
+        }
+    }
+})
+if(objectStats.m <= 20) {
+    createActionButton("Look for old money", (button) => {
+        button.onclick = function (ev) {
+            alert("You found 5 dollars!");
+            objectStats.m = +objectStats.m+5;
+            updateStats();
+            button.remove();
+        }
+    })
+}
+baaaa = document.createElement("br")
+baaaa.id = "aa";
+caaaa = document.createElement("br")
+daaaa = document.createElement("h2")
+daaaa.appendChild(document.createTextNode("Danger Zone"))
+document.body.appendChild(baaaa)
+document.body.appendChild(caaaa)
+document.body.appendChild(daaaa)
 createActionButton("Reset", (button) => {
     button.onclick = (event) => {
         cc = confirm("You are about to lose all of your progress in Detroit Adventure.\nAre you sure?")
@@ -177,21 +237,15 @@ createActionButton("Export Save", (button) => {
         alert(localStorage.getItem("detroit_stats"))  
     }
 })
-
-if(objectStats.m == 0) {
-    createActionButton("Look for old money", (button) => {
-        button.onclick = function (ev) {
-            alert("You found 5 dollars!");
-            objectStats.m = +objectStats.m+5;
-            updateStats();
-            button.remove();
-        }
-    })
+if(objectStats.at == "Ohio") {
+    OhioStart();
 }
+
+// Job Logic
 
 setInterval(function () {
     if(objectStats.job != "None") {
-        objectStats.m = +objectStats.m+2;
+        objectStats.m = +objectStats.m+4;
         updateStats();
     }
     if(objectStats.job == "OK Games") {
@@ -211,3 +265,8 @@ if(objectStats.job != "None") {
     newJob(objectStats.job)
 }
 
+
+
+versionElement = document.createElement("p")
+versionElement.appendChild(document.createTextNode("Detroit Adventure v"+version))
+document.body.appendChild(versionElement)
--- ohio.js
+++ ohio.js
@@ -1,6 +1,30 @@
 function weGoinToOhio() {
+    objectStats.at = "Ohio";
+    updateStats();
+    OhioShop();
+}
+
+function OhioStart() {
+    objectStats.loc = "Ohio";
+    updateStats();
+}
+
+function OhioShop() {
+    objectStats.loc = "Shop (Ohio)"
     addShopItem({name:"Job At OK Games",cost:50,limit:1}, (ev) => {
         objectStats.job = "OK Games";
         updateStats();
     })
+    addShopItem({name:"Ticket back to Detroit",cost:150,limit:1}, (event) => {
+        alert("You're going to Detroit.")
+        objectStats.loc = "Your Home (Detroit)";
+        objectStats.at = "Detroit";
+        updateStats();
+        window.location.reload();
+    })
+}
+
+function ExitOhioShop() {
+    objectStats.loc = "Ohio";
+    updateStats();
 }
