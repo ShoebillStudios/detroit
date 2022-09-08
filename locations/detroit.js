9d26325d623b57d9f49f6792fabd68dd2079a036
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-08 13:14:54 -0500

    move functions

--- locations/detroit.js
+++ locations/detroit.js
@@ -23,106 +23,7 @@ if(welcome == true) {
 var gameDiv = document.getElementById("game");
 var statsDiv = document.getElementById("stats");
 updateStats();
-function updateStats() {
-    statsDiv.innerHTML = `
-<p>Money: D\$${objectStats.m}</p>
-<p>IQ: ${objectStats.iq}</p>
-<p>Attack: ${objectStats.att}</p>
-<p>Defense: ${objectStats.def}</p>
-<p>Health: ${objectStats.h}</p>`;
-c = `detroit:${objectStats.detroit};inv:${objectStats.inv};name:${objectStats.name};iq:${objectStats.iq};att:${objectStats.att};def:${objectStats.def};h:${objectStats.h};m:${objectStats.m};hwid:${objectStats.hwid};loc:${objectStats.loc};job:${objectStats.job};at:${objectStats.at}`
-localStorage.setItem("detroit_stats", c)
-}
-function createActionButton(action, callback) {
-    if(document.getElementById("action:"+action)) {
-        document.getElementById("action:"+action).remove();
-    }
-    a = document.createElement("button")
-    a.appendChild(document.createTextNode(action))
-    a.id = "action:"+action
-    document.body.appendChild(a)
-    callback(a);
-    return a;
-}
 
-function openShop() {
-    if(document.getElementById("shop")) {
-        document.getElementById("shop").remove();
-        objectStats.loc = "Your Home (Detroit)"
-        updateStats();
-        if(objectStats.at == "Ohio") {
-            ExitOhioShop();
-        }
-        return;
-    }
-    gameDiv.innerHTML += `<div id="shop"><h1>Shop</h1></div>`
-    if(objectStats.at == "Ohio") {
-        OhioShop();
-        return;
-    }
-    addShopItem({name:"Ticket to Ohio",cost:50,limit:1}, (event) => {
-        alert("You're going to Ohio.")
-        objectStats.loc = "Ohio";
-        objectStats.at = "Ohio";
-        updateStats();
-        weGoinToOhio();
-    })
-    addShopItem({name:"Job at FloorStore",cost:10,limit:1}, (event) => {
-        alert("Congratulations, you got the job.")
-        objectStats.job = "FloorStore";
-        updateStats();
-    })
-    addShopItem({name:"Balazation 5",cost:500,limit:2}, (event) => {
-        alert("YOOO YOU GOT THE BALASTATION 5...")
-        createActionButton("Play your Balazation5 "+Math.random().toString().substring(2,3), (button) => {
-            button.onclick = function (event) {
-                alert("You've won the DutyCal Tournament!!!")
-                objectStats.m = +objectStats.m + 5000;
-                updateStats();
-                button.remove();
-            }
-        })
-    })
-}
-
-function addShopItem(item={name:"My Item", cost:5,limit:3}, callback=(button)=>{}) {
-    var abc = 0;
-    objectStats.inv.split(',').forEach(invItem => {
-        if(invItem == item.name) {
-            abc++;
-        }
-    })
-    if(abc == item.limit) {
-        return;
-    }
-    b = document.createElement('button')
-    b.appendChild(document.createTextNode(item.name+": D$"+item.cost+" - Limit: "+item.limit))
-    document.getElementById("shop").appendChild(b)
-    b.onclick = function (event) {
-        buyItemFromShop(item, callback);
-    }
-    return b;
-}
-
-function buyItemFromShop(item, callback) {
-    abc = 0;
-    objectStats.inv.split(",").forEach(itemc => {
-        if(itemc == item.name) {
-            abc++;
-        }
-    })
-    if(item.limit <= abc) {
-        return;
-    }
-    if(objectStats.m >= item.cost) {
-        objectStats.m = objectStats.m - item.cost;
-        objectStats.inv += ","+item.name
-        updateStats();
-        openShop();
-    openShop();
-    callback();
-    }
-}
 
 // Actions at the bottom of the screen
 
@@ -233,13 +134,6 @@ setInterval(function () {
     objectStats.m = +objectStats.m+passive;
         updateStats();
 },2500)
-
-function chance(chanceType) {
-    a = Math.random().toString().substring(2,4);
-    if(a <= chances[chanceType]) {
-        
-    }
-}
 
 if(objectStats.job != "None") {
     newJob(objectStats.job)
@@ -361,3 +255,111 @@ function inventorySort() {
 versionElement = document.createElement("p")
 versionElement.appendChild(document.createTextNode("Detroit Adventure v"+version))
 document.body.appendChild(versionElement)
+
+// Functions
+function updateStats() {
+    statsDiv.innerHTML = `
+<p>Money: D\$${objectStats.m}</p>
+<p>IQ: ${objectStats.iq}</p>
+<p>Attack: ${objectStats.att}</p>
+<p>Defense: ${objectStats.def}</p>
+<p>Health: ${objectStats.h}</p>`;
+c = `detroit:${objectStats.detroit};inv:${objectStats.inv};name:${objectStats.name};iq:${objectStats.iq};att:${objectStats.att};def:${objectStats.def};h:${objectStats.h};m:${objectStats.m};hwid:${objectStats.hwid};loc:${objectStats.loc};job:${objectStats.job};at:${objectStats.at}`
+localStorage.setItem("detroit_stats", c)
+}
+function createActionButton(action, callback) {
+    if(document.getElementById("action:"+action)) {
+        document.getElementById("action:"+action).remove();
+    }
+    a = document.createElement("button")
+    a.appendChild(document.createTextNode(action))
+    a.id = "action:"+action
+    document.body.appendChild(a)
+    callback(a);
+    return a;
+}
+
+function openShop() {
+    if(document.getElementById("shop")) {
+        document.getElementById("shop").remove();
+        objectStats.loc = "Your Home (Detroit)"
+        updateStats();
+        if(objectStats.at == "Ohio") {
+            ExitOhioShop();
+        }
+        return;
+    }
+    gameDiv.innerHTML += `<div id="shop"><h1>Shop</h1></div>`
+    if(objectStats.at == "Ohio") {
+        OhioShop();
+        return;
+    }
+    addShopItem({name:"Ticket to Ohio",cost:50,limit:1}, (event) => {
+        alert("You're going to Ohio.")
+        objectStats.loc = "Ohio";
+        objectStats.at = "Ohio";
+        updateStats();
+        weGoinToOhio();
+    })
+    addShopItem({name:"Job at FloorStore",cost:10,limit:1}, (event) => {
+        alert("Congratulations, you got the job.")
+        objectStats.job = "FloorStore";
+        updateStats();
+    })
+    addShopItem({name:"Balazation 5",cost:500,limit:2}, (event) => {
+        alert("YOOO YOU GOT THE BALASTATION 5...")
+        createActionButton("Play your Balazation5 "+Math.random().toString().substring(2,3), (button) => {
+            button.onclick = function (event) {
+                alert("You've won the DutyCal Tournament!!!")
+                objectStats.m = +objectStats.m + 5000;
+                updateStats();
+                button.remove();
+            }
+        })
+    })
+}
+
+function addShopItem(item={name:"My Item", cost:5,limit:3}, callback=(button)=>{}) {
+    var abc = 0;
+    objectStats.inv.split(',').forEach(invItem => {
+        if(invItem == item.name) {
+            abc++;
+        }
+    })
+    if(abc == item.limit) {
+        return;
+    }
+    b = document.createElement('button')
+    b.appendChild(document.createTextNode(item.name+": D$"+item.cost+" - Limit: "+item.limit))
+    document.getElementById("shop").appendChild(b)
+    b.onclick = function (event) {
+        buyItemFromShop(item, callback);
+    }
+    return b;
+}
+
+function buyItemFromShop(item, callback) {
+    abc = 0;
+    objectStats.inv.split(",").forEach(itemc => {
+        if(itemc == item.name) {
+            abc++;
+        }
+    })
+    if(item.limit <= abc) {
+        return;
+    }
+    if(objectStats.m >= item.cost) {
+        objectStats.m = objectStats.m - item.cost;
+        objectStats.inv += ","+item.name
+        updateStats();
+        openShop();
+    openShop();
+    callback();
+    }
+}
+function chance(chanceType) {
+    a = Math.random().toString().substring(2,4);
+    if(a <= chances[chanceType]) {
+        
+    }
+}
